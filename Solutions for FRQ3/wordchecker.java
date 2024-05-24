028cbe28c610a7c01c5e1c1a8f0d7831efd8454d
w1iang <chelsealiang88@gmail.com>
2024-05-24 10:08:00 -0400

    Update wordchecker.java

--- Solutions for FRQ3/wordchecker.java
+++ Solutions for FRQ3/wordchecker.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class WordChecker {
+  private ArrayList<String> wordList;
+
+  public WordChecker(ArrayList<String> words) {
+    wordList = words;
+  }
+
+  public boolean isWordChain() {
+    for(int i = 0; i < wordList.size()-2; i++) {
+      if (wordList.get(i+1).indexOf(wordList.get(i))!=1){
+        return true;
+      }
+    }
+      return false;
+  }
+
+
+  public ArrayList<String> createList(String target) {
+    ArrayList<String> result = new ArrayList<String>();
+    for(int j = 0; j < wordList.size(); j++){
+      if (wordList.get(j).indexOf(target)==0) {
+        result.add(wordList.get(j).substring(target.length()));
+      }
+    }
+    return result;             
+  }
+}
