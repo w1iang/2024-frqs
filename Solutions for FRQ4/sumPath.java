db9339a1eec1fb9118bdad65d99f0a2555093210
w1iang <chelsealiang88@gmail.com>
2024-05-29 00:16:57 -0400

    Update sumPath.java

--- Solutions for FRQ4/sumPath.java
+++ Solutions for FRQ4/sumPath.java
@@ -1,1 +1,19 @@
+public int sumPath(int row, int col)
+{
+    int total = grid[row][col];
+    Location j = getNextLoc(row, col);
 
+    while(j != null)
+    {
+        total += grid[j.getRow()][j.getCol()]; 
+
+        if(j.getRow() < grid.length - 1 || j.getCol() < grid[0].length - 1){
+          j = getNextLoc(loc.getRow(), loc.getCol()); }
+            
+        else {
+            loc = null;
+        }   
+    }
+
+    return sum;
+}
